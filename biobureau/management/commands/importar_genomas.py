c45dd0eb660ac9180f4671efdc686bc277ae90ab
Felipe Ferreira <felipe.gomes.ferreira@gmail.com>
2018-09-16 23:01:03 -0300

    Varios bugs, entender o que aconteceu com o GI

--- biobureau/management/commands/importar_genomas.py
+++ biobureau/management/commands/importar_genomas.py
@@ -15,7 +15,9 @@ from biobureau.models import (
     Algoritmo,
     Database,
     Sequenciamento,
-    Sequencia
+    Sequencia,
+    GeneInfo,
+    GeneOntology
 )
 from listagens.models import (
     Biodiversidade
@@ -23,8 +25,6 @@ from listagens.models import (
 
 @memoized
 def findfamily(genus, species):
-
-    print(f"Planteando {genus} {species}")
     address = f'http://www.theplantlist.org/tpl1.1/search?q={genus}%20{species}&csv=true'
 
     response = requests.get(
@@ -138,9 +138,6 @@ def gerar_especie_aliquota(genero, especie, algoritmo, database):
     return n_sequenciamento
 
 
-
-
-
 @memoized
 def obter_especie(familia, genero, especie):
     n_familia = Familia.objects.filter(nome=familia).first()
@@ -175,16 +172,100 @@ def obter_especie(familia, genero, especie):
     return n_especie
 
 
+@memoized
+def obter_gi(gi):
+    gi = int(gi)
+    n_gi = GeneInfo.objects.filter(
+        identificacao=gi
+    ).first()
+
+    if not n_gi:
+        n_gi = GeneInfo()
+        n_gi.identificacao = gi
+        n_gi.save()
+
+    return n_gi
+
+
+@memoized
+def obter_gis(stringgi):
+    gis = stringgi.split(';')
+    gis = [x.strip() for x in gis]
+    return [obter_gi(gi) for gi in gis]
+
+
+@memoized
+def obter_go(go):
+    n_go = GeneOntology.objects.filter(
+        nome = go
+    ).first()
+
+    if not n_go:
+        n_go = GeneOntology()
+        n_go.nome = go
+        n_go.save()
+    
+    return n_go
+
+
+@memoized
+def obter_gos(stringgo):
+    gos = stringgo.split()
+    gos = [x.strip() for x in gos]
+    return [obter_go(go) for go in gos]
+
+
 def limpabanco():
-    pass
+    Sequencia.objects.all().delete()
+
+
+def incluir_sequencia(sequenciamento, biodiversidade):
+    n_sequencia = Sequencia()
+    n_sequencia.sequenciamento = sequenciamento
+    n_sequencia.qseqid = biodiversidade.qseqid
+    n_sequencia.sseqid = biodiversidade.sseqid
+    n_sequencia.pident = biodiversidade.pident
+    n_sequencia.length = biodiversidade.length
+    n_sequencia.mismatch = biodiversidade.mismatch
+    n_sequencia.gapopen = biodiversidade.gapopen
+    n_sequencia.qstart = biodiversidade.qstart
+    n_sequencia.qend = biodiversidade.qend
+    n_sequencia.sstart = biodiversidade.sstart
+    n_sequencia.send = biodiversidade.send
+    n_sequencia.evalue = biodiversidade.evalue
+    n_sequencia.bitscore = biodiversidade.bitscore
+    n_sequencia.geneid = biodiversidade.geneid
+    n_sequencia.ncbitaxon = biodiversidade.ncbitaxon
+    n_sequencia.ltaxon = biodiversidade.ltaxon
+    n_sequencia.repid = biodiversidade.repid
+    n_sequencia.clustername = biodiversidade.clustername
+    n_sequencia.station = biodiversidade.station
+    n_sequencia.save()
+
+    if biodiversidade.gi and biodiversidade.gi.strip():
+        n_sequencia.geneinfo.add(
+            *obter_gis(
+                biodiversidade.gi
+            )
+        )
+
+    if biodiversidade.obs and biodiversidade.obs.strip():
+        n_sequencia.geneontology.add(
+            *obter_gos(
+                biodiversidade.obs
+            )
+        )
+
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
+        limpabanco()
         print('Contando biodiversidades')
-        nbio = 100 # Biodiversidade.objects.all().count()
+        items = Biodiversidade.objects.filter(algorithm='diamond')
+        nbio = 100 # items.count()
         print('Começando a brincadeira')
-        for i in tqdmrange(nbio):
-            biodiversidade = Biodiversidade.objects.all()[i]
+        for i in tqdm(range(nbio)):
+            biodiversidade = items[i]
 
             genero, especie = biodiversidade.species.split()
             sequenciamento = gerar_especie_aliquota(
@@ -193,6 +274,8 @@ class Command(BaseCommand):
                 biodiversidade.algorithm,
                 biodiversidade.database)
             
-
-
+            incluir_sequencia(
+                sequenciamento,
+                biodiversidade
+            )
 
