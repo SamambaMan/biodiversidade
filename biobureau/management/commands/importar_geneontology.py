35e040275c5ce11ee12bd10b680fe845bdb6135b
Felipe Ferreira <felipe.gomes.ferreira@gmail.com>
2018-11-09 21:22:54 -0200

    importacao em paralelo

--- biobureau/management/commands/importar_geneontology.py
+++ biobureau/management/commands/importar_geneontology.py
@@ -3,7 +3,13 @@ from tqdm import tqdm
 from django.core.management import BaseCommand
 from memoized import memoized
 from biobureau.models import GeneOntology
+from multiprocessing.dummy import Pool
 
+def importar(linha):
+    go = GeneOntology.objects.filter(nome=linha[0]).first()
+    if go:
+        go.descricao = linha[1]
+        go.save()
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
@@ -13,12 +19,17 @@ class Command(BaseCommand):
         print('Contando Linhas: ', end='')
         num_lines = sum([1 for line in open(options['arquivo'][0])])
         print(num_lines)
+
+        pool = Pool(10)
+
         with open(options['arquivo'][0], 'r') as file:
             reader = csv.reader(file, delimiter='\t')
 
-            for linha in tqdm(reader, total=num_lines):
-                go = GeneOntology.objects.filter(nome=linha[0]).first()
-                if go:
-                    go.descricao = linha[1]
-                    go.save()
-            
+            list(
+                tqdm(
+                    pool.imap(
+                        importar,
+                        reader
+                    ),
+                    total=num_lines)
+            )
